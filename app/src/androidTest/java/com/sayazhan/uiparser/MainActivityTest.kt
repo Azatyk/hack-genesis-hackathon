89b360214d3593a76cbf46dd56dfc2114d8d39da
Crepiks <sayazhan.onlassyn@mail.ru>
2021-04-18 12:26:41 +0600

    feat: implement espresso test for main activity

---
+++ app/src/androidTest/java/com/sayazhan/uiparser/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package com.sayazhan.uiparser
+
+import android.util.Log
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+private const val TAG = "MainActivityTest"
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()))
+    }
+}
